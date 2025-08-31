2e57bcc518842640af3991b7e8182b6024d35525
Dongyoung <58430409+katalria@users.noreply.github.com>
2025-08-31 20:46:01 +0900

    fix model path (#8)
    * feat : Add packages for langchain and set init orchestrator prompt
    
    * chore : change folder structure
    
    * chore : add package
    
    * feat : streamlit 기반 기초 서비스 구현 및 모델 연결
    
    * exp : dongyoung test for agent
    
    * fix : model path
    
    * fix : fix medblip path for service container
    
    ---------
    
    Co-authored-by: 김동영 <dongyoung@gimdong-yeong-ui-Macmini.local>

--- app/main.py
+++ app/main.py
@@ -29,7 +29,8 @@ def load_medblip_model():
     
     # 가능한 모델 경로들 (우선순위 순)
     possible_paths = [
-        "/app/model/model.safetensors",  # Docker 컨테이너 내부 경로
+        "/app/model",  # Docker 컨테이너 내부 경로
+
     ]
     
     for model_path in possible_paths:
